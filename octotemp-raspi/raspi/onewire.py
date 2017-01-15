36981ff57f1ab4b99a77a5939af7ad03d1266ced
cmbg78 <cmbg78>
2017-01-15 16:59:04 -0500

    update

---
+++ octotemp-raspi/raspi/onewire.py
@@ -0,0 +1,10 @@
+import subprocess as sub
+import sys
+
+def getDeviceList():
+    try:
+        devices = sub.check_output(["ls", "/sys/bus/w1/devices"])
+        devices = devices.split('\n')
+    except:
+        devices = "An error was encountered - do you have OneWire probes attached?  error: %s" % sys.exc_info()[0]    
+    return devices
