c75226f9588761d1bcf01fddacb1d811b75e504d
cmbg78 <cmbg78>
2017-01-15 17:05:27 -0500

    update

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@
 plugin_identifier = "octotemp"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octotemp-raspi"
+plugin_package = "octoprint_octotemp"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "octotemp-raspi"
+plugin_name = "octotemp"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
 plugin_version = "0.1.0"
