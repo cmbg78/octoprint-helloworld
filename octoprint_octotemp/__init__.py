65222faede1f7c50a9b064ccb069058a9fa7acce
cmbg78 <cmbg78>
2017-01-21 10:59:53 -0500

    cleaned up import for onewire

--- octoprint_octotemp/__init__.py
+++ octoprint_octotemp/__init__.py
@@ -4,20 +4,25 @@ from __future__ import absolute_import
 import octoprint.plugin
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(sys.path[len(sys.path)-1], 'octoprint_octotemp', 'raspi')))
+import imp
+
+# import included onewire-raspi module
+sys.path.append(os.path.abspath(os.path.join(os.getcwd(), 'octoprint_octotemp', 'raspi')))
 import onewire
 
 class OctotempPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlugin,octoprint.plugin.SettingsPlugin,octoprint.plugin.AssetPlugin):
+
+    def __init__(self):
+        self.probeData = {}
+
     def on_after_startup(self):
         self._logger.info("octotemp-raspi plugin loaded")
-        self._logger.info(self._settings.get(["probe_0"]))
-
+        #self._logger.info(sys.path)
+        
     def get_settings_defaults(self):
         probes = onewire.getDeviceList()
-        if len(probes) > 10:
-            self._logger.warn("The Octotemp plugin only supports up to 10 OneWire probes. Only the first 10 probes enumerated will be avaialble for use.")
-        pd = {}
         i = 0
+
         for probe in probes:
             if "_bus_" in probe:
                 probes.remove(probe)
@@ -25,12 +30,11 @@ class OctotempPlugin(octoprint.plugin.StartupPlugin,octoprint.plugin.TemplatePlu
                 pname = "probe_" + str(i)
                 pdata = [probe, '']
                 d = {pname : pdata}
-                pd.update(d)
-                #f = {probe.replace("-", "_") : ""}
-                #pd.update(f)
-            i = i+1
-        self._logger.info(str(pd))
-        return pd
+                self.probeData.update(d)
+            i = i + 1
+        self._logger.info("probeData = " + str(self.probeData))
+        return self.probeData
+        
     def get_template_configs(self):
         return [
             dict(type="settings", custom_bindings=False)
